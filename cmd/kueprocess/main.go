e30014d8811d5868f69f13e5bd170efe05964b61
Lisa <78325361+lisaantal@users.noreply.github.com>
2025-10-15 20:28:08 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/kueprocess/main.go
@@ -0,0 +1,23 @@
+// cmd/kueprocess/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "kueprocess/internal/kueprocess"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := kueprocess.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
